6caa8b20d16fe1e08a4bc7350bb3cec40885dd6d
katsuyoshi ozaki <ozaki@datasign.jp>
2024-01-26 14:51:02 +0900

    Fixed to specify data type of column

--- synapse/storage/schema/main/delta/85/01_create_table_vp_session_management.sql
+++ synapse/storage/schema/main/delta/85/01_create_table_vp_session_management.sql
@@ -15,7 +15,7 @@
 
 CREATE TABLE IF NOT EXISTS vp_session_management (
     sid TEXT PRIMARY KEY,
-    vp_type NOT NULL, -- ageOver13 or affiliation or ...
+    vp_type TEXT NOT NULL, -- ageOver13 or affiliation or ...
     status TEXT NOT NULL,
     ro_nonce TEXT NOT NULL,
     created_ts BIGINT NOT NULL
